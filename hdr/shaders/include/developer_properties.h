e755fbb0f282a014c52358bbec64205169974f4c
MajorPainTheCactus <thedailytalenewspaper@gmail.com>
2022-02-06 21:59:44 +0000

    Refactored shader to support shadow and dot masks - added crt-shadow-mask-hdr.slang, crt-dot-mask-hdr.slang Added Bang & Olufsen TV preset (shadow mask) Added Toshiba Microfilter TV preset (dot mask) Renamed crt-sony-pvm-4k-hdr.slang to more general crt-aperture-grille-hdr.slang Moved header files into an include directory

---
+++ hdr/shaders/include/developer_properties.h
@@ -0,0 +1,28 @@
+
+
+   // Developer Settings
+   float CRTResolution;
+   
+   // Vertical Settings
+   float RedScanlineMin;
+   float RedScanlineMax;
+   float RedScanlineAttack;
+   float GreenScanlineMin;
+   float GreenScanlineMax;
+   float GreenScanlineAttack;
+   float BlueScanlineMin;
+   float BlueScanlineMax;
+   float BlueScanlineAttack;
+
+   // Horizontal Settings
+   float RedBeamSharpness;
+   float RedBeamAttack;
+   float GreenBeamSharpness;
+   float GreenBeamAttack;
+   float BlueBeamSharpness;
+   float BlueBeamAttack;
+
+#if WHITE_BALANCE_CONTROL
+   float WhiteTemperature;
+   float WhiteTint;
+#endif // WHITE_BALANCE_CONTROL
