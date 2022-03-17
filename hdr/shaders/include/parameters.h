da1a96f04ea8d312db558d69eaa2db39871fbe59
MajorPainTheCactus <thedailytalenewspaper@gmail.com>
2022-03-17 21:26:30 +0000

    Added 4K 300TVL support (8K still to do) Added Sony PVM 1910 simulation Added Virtua Fighter Arcade simulation (still a little early) Cleaned up parameters with uneeded/confusing instructions

--- hdr/shaders/include/parameters.h
+++ hdr/shaders/include/parameters.h
@@ -7,11 +7,6 @@
 #pragma parameter hcrt_support2                      "MIN SPEC: DisplayHDR 600, 4K, RetroArch v1.10"             0.0      0.0   0.0001   0.0
 #pragma parameter hcrt_support3                      "REC SPEC: DisplayHDR 1000, 4K+, RetroArch v1.10"           0.0      0.0   0.0001   0.0
 #pragma parameter hcrt_space1                        " "                                                         0.0      0.0   0.0001   0.0
-#pragma parameter hcrt_instructions0                 "HDR: Enable HDR: On"                                       0.0      0.0   0.0001   0.0
-#pragma parameter hcrt_instructions1                 "SCALING: Integer Scale: ON"                                0.0      0.0   0.0001   0.0
-#pragma parameter hcrt_instructions2                 "SCALING: Integer Overscale: ON"                            0.0      0.0   0.0001   0.0
-#pragma parameter hcrt_instructions3                 "SCALING: Apect Ratio: Core Provided"                       0.0      0.0   0.0001   0.0
-#pragma parameter hcrt_space2                        " "                                                         0.0      0.0   0.0001   0.0
 #pragma parameter hcrt_user_settings                 "USER SETTINGS:"                                            0.0      0.0   0.0001   0.0
 #pragma parameter hcrt_hdr                           "    HDR | SDR"                                             0.0      0.0   1.0      1.0
 #pragma parameter hcrt_max_nits                      "    HDR: Display's Peak Luminance"                         700.0    0.0   10000.0  10.0
@@ -29,7 +24,7 @@
 #pragma parameter hcrt_space3                        " "                                                         0.0      0.0   0.0001   0.0
 #pragma parameter hcrt_developer_settings            "DEVELOPER SETTINGS:"                                       0.0      0.0   0.0001   0.0
 #pragma parameter hcrt_crt_screen_type               "    CRT Type: APERTURE GRILLE | SHADOW MASK | SLOT MASK"   0.0      0.0   2.0      1.0
-#pragma parameter hcrt_crt_resolution                "    CRT Resolution: 600TVL | 800TVL | 1000TVL"             0.0      0.0   2.0      1.0
+#pragma parameter hcrt_crt_resolution                "    CRT Resolution: 300TVL | 600TVL | 800TVL | 1000TVL"    1.0      0.0   3.0      1.0
 #pragma parameter hcrt_colour_system                 "    CRT Colour System: PAL | NTSC-U | NTSC-J"              1.0      0.0   2.0      1.0
 #pragma parameter hcrt_white_temperature             "    White Point: (PAL:D65, NTSC-U:D65, NTSC-J:D93)"        0.0      -5000.0  12000.0      100.0
 #pragma parameter hcrt_expand_gamut                  "    HDR: Original/Vivid"                                   0.0      0.0   1.0      1.0
