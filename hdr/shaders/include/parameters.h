34e0fb10d24577c6a15641bd4f94809b33e3ce24
MajorPainTheCactus <thedailytalenewspaper@gmail.com>
2022-09-22 23:22:50 +0100

    Added mask accurate, colour accurate switch which effects HDR and DCI-P3 to parameters so that users can choose whether they want 100% pitch black mask or more correct looking colours i.e. not so yellow-y looking greens. Defaults to latter which is the original behaviour.

--- hdr/shaders/include/parameters.h
+++ hdr/shaders/include/parameters.h
@@ -13,6 +13,7 @@
 #pragma parameter hcrt_space1                        " "                                                            0.0      0.0   0.0001   0.0001
 #pragma parameter hcrt_user_settings                 "YOUR DISPLAY'S SETTINGS:"                                     0.0      0.0   0.0001   0.0001
 #pragma parameter hcrt_hdr                           "    SDR | HDR"                                                1.0      0.0   1.0      1.0
+#pragma parameter hcrt_colour_accurate               "    Mask Accurate/Colour Accurate"                            1.0      0.0   1.0      1.0
 #pragma parameter hcrt_colour_space                  "    SDR: Display's Colour Space: r709 | sRGB | DCI-P3"        1.0      0.0   2.0      1.0
 #pragma parameter hcrt_gamma_out                     "    SDR: Gamma"                                               2.4      1.0   5.0      0.01
 #pragma parameter hcrt_max_nits                      "    HDR: Display's Peak Luminance"                            700.0    0.0   10000.0  10.0
